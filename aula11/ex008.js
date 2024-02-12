f2cccb55a440be3aedbceae6c9614e802a7e6d5e
oJaciel <149974137+oJaciel@users.noreply.github.com>
2024-02-11 21:22:39 -0300

    Exercícios da Aula 11 + Atualização das Anotações

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+//Exercício da aula de condições (parte 1)
+//Exercício feito para ser executado diretamente no NodeJS
+
+//Teste de condicionais, utilizando exemplo de comparador de limite de velocidade, verificando se velocidade está acima da permitida
+
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { //Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre com cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+//Exercício da aula de condições (parte 1)
+//Exercício feito para ser executado diretamente no NodeJS
+
+//Teste de condicionais, utilizando sistema que mostra se é brasileiro ou estrangeiro, baseado no país que reside
+
+var país = 'EUA'
+
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro`)
+}
