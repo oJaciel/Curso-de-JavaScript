51bfe7e9d55385148cae8e24777a5f501857ef27
oJaciel <149974137+oJaciel@users.noreply.github.com>
2024-02-19 21:15:17 -0300

    Arquivos da Aula 13 + Atualização das Anotações

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+
+/*
+while (contador <= 500) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
